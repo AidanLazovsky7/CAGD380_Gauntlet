12db49a75be33167240af9273355c64db88596cb
AidanLazovsky7 <103057135+AidanLazovsky7@users.noreply.github.com>
2024-04-16 13:46:54 -0700

    Walls are prototyped

--- 380_Gauntlet/Assets/Scripts/BreakableWall.cs
+++ 380_Gauntlet/Assets/Scripts/BreakableWall.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class BreakableWall : MonoBehaviour, iDamageable
 {
-    [Tooltip("These are the prefabs for the different states the Wall can be in")]
+    [Tooltip("These are the prefabs for the different states the Wall can be in, there are a max of 3")]
     [SerializeField]
     private List<GameObject> _wallStates = new List<GameObject>();
 
-    private int _currentLevel;
+    [Tooltip("Set how many hits you want the wall to take here, it should be less than the amount of wall states")]
+    [Range(0,2)]
+    [SerializeField]
+    private int _currentLevel = -1;
+
+    private GameObject _currentWall;
+
+    private void Start()
+    {
+        if (_currentLevel == -1) _currentLevel = _wallStates.Count - 1;
+       
+        
+       _currentWall = Instantiate(_wallStates[_currentLevel], transform.position, transform.rotation);
+    }
 
     public void TakeDamage(float damage)
     {
@@ -18,8 +31,20 @@ public class BreakableWall : MonoBehaviour, iDamageable
     public void OnDeath()
     {
         _currentLevel--;
-        if (_currentLevel >= 0) Instantiate(_wallStates[_currentLevel], transform.position, transform.rotation);
-        Destroy(this.gameObject);
+        GameObject temp = _currentWall;
+        if (_currentLevel >= 0)
+        {
+            _currentWall = Instantiate(_wallStates[_currentLevel], transform.position, transform.rotation);
+        }
+        Destroy(temp);
+        this.gameObject.SetActive(false);
     }
 
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Hit Wall"))
+        {
+            TakeDamage(1);
+        }
+    }
 }
