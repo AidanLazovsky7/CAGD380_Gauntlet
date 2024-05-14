5f7d2d6a8e3a34f4e03e9134762cbf0f94450dd7
msbruschke <103210835+msbruschke@users.noreply.github.com>
2024-05-13 23:08:22 -0700

    breakable potions

--- 380_Gauntlet/Assets/Scripts/Collectables/Food.cs
+++ 380_Gauntlet/Assets/Scripts/Collectables/Food.cs
@@ -2,13 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Food : MonoBehaviour, iCollectable
+public class Food : MonoBehaviour, iCollectable, iDamageable
 {
+    public bool breakable = false;
+
     private int _healing = 100;
 
     public void pickup(Player user)
     {
         user.addHealth(_healing);
+        Destroy(this.gameObject);
+    }
+
+    public void TakeDamage(int damage, AttackType atkType)
+    {
+        if (breakable)
+        {
+            OnDeath();
+        }
+    }
+
+    public void OnDeath()
+    {
         Destroy(this.gameObject);
     }
 }
